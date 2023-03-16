3ad8be765f1f925866517c0f1518e7355a1c28f7
jgcodes2020 <jackyguo18@hotmail.com>
2023-03-16 09:46:49 -0400

    First commit

---
+++ Mupen64PlusRR/Controls/VidextControl.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+using Avalonia.Media;
+
+namespace Mupen64PlusRR.Controls;
+
+public class VidextControl : Control
+{
+    public override void Render(DrawingContext context)
+    {
+        base.Render(context);
+    }
+}
---
+++ Mupen64PlusRR/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Mupen64PlusRR.ViewModels
+{
+    public class MainWindowViewModel : ViewModelBase
+    {
+        public string Greeting => "Welcome to Avalonia!";
+    }
+}
